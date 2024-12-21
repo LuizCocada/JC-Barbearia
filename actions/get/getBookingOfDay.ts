43f21b04b6ccaaa9a5b75ea3bd3eb901324a8873
luizribeiro <ludehenriq@gmail.com>
2024-12-21 14:23:51 -0300

    ajusta a lógica de recuperação de agendamentos para utilizar moment-timezone e calcular corretamente os limites do dia em UTC

--- actions/get/getBookingOfDay.ts
+++ actions/get/getBookingOfDay.ts
@@ -1,13 +1,9 @@
-"use server";
-
-import { db } from "@/lib/prisma";
+import { db } from '@/lib/prisma';
+import moment from 'moment-timezone';
 
 export const GetBookingOfDay = async () => {
-  const startOfToday = new Date();
-  startOfToday.setHours(0, 0, 0, 0);
-
-  const endOfToday = new Date();
-  endOfToday.setHours(23, 59, 59, 999);
+  const startOfToday = moment().tz('UTC').startOf('day').toDate();
+  const endOfToday = moment().tz('UTC').endOf('day').toDate();
 
   return await db.booking.findMany({
     where: {
@@ -25,6 +21,57 @@ export const GetBookingOfDay = async () => {
     },
   });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use server";
+
+// import { db } from "@/lib/prisma";
+
+// export const GetBookingOfDay = async () => {
+//   const startOfToday = new Date();
+//   startOfToday.setHours(0, 0, 0, 0);
+
+//   const endOfToday = new Date();
+//   endOfToday.setHours(23, 59, 59, 999);
+
+//   return await db.booking.findMany({
+//     where: {
+//       date: {
+//         gte: startOfToday,
+//         lte: endOfToday,
+//       },
+//     },
+//     include: {
+//       service: true,
+//       user: true,
+//     },
+//     orderBy: {
+//       date: "asc",
+//     },
+//   });
+// };
 
 
 
