38ff99f249edde6f3595673ba56758001c44338f
luizribeiro <ludehenriq@gmail.com>
2024-12-21 14:45:26 -0300

    refatora a lógica de recuperação de agendamentos para utilizar objetos Date em vez de moment-timezone

--- actions/get/getBookingOfDay.ts
+++ actions/get/getBookingOfDay.ts
@@ -1,9 +1,13 @@
-import { db } from '@/lib/prisma';
-import moment from 'moment-timezone';
+"use server";
+
+import { db } from "@/lib/prisma";
 
 export const GetBookingOfDay = async () => {
-  const startOfToday = moment().tz('UTC').startOf('day').toDate();
-  const endOfToday = moment().tz('UTC').endOf('day').toDate();
+  const startOfToday = new Date();
+  startOfToday.setHours(0, 0, 0, 0);
+
+  const endOfToday = new Date();
+  endOfToday.setHours(23, 59, 59, 999);
 
   return await db.booking.findMany({
     where: {
@@ -21,57 +25,6 @@ export const GetBookingOfDay = async () => {
     },
   });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use server";
-
-// import { db } from "@/lib/prisma";
-
-// export const GetBookingOfDay = async () => {
-//   const startOfToday = new Date();
-//   startOfToday.setHours(0, 0, 0, 0);
-
-//   const endOfToday = new Date();
-//   endOfToday.setHours(23, 59, 59, 999);
-
-//   return await db.booking.findMany({
-//     where: {
-//       date: {
-//         gte: startOfToday,
-//         lte: endOfToday,
-//       },
-//     },
-//     include: {
-//       service: true,
-//       user: true,
-//     },
-//     orderBy: {
-//       date: "asc",
-//     },
-//   });
-// };
 
 
 
