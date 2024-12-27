43aeeb28c6d87f031d68f140dadf133ac8bb0ee1
luizribeiro <ludehenriq@gmail.com>
2024-12-27 16:48:33 -0300

    refactor: atualizar a lógica de obtenção de agendamentos do dia para usar time zone e conversão UTC

--- actions/get/getBookingOfDay.ts
+++ actions/get/getBookingOfDay.ts
@@ -1,20 +1,23 @@
 "use server";
 
 import { db } from "@/lib/prisma";
-import { formatInTimeZone, toDate } from 'date-fns-tz';
+import { toZonedTime, fromZonedTime } from 'date-fns-tz';
 
 export const GetBookingOfDay = async () => {
   const timeZone = 'America/Sao_Paulo';
 
   const now = new Date();
-  const startOfToday = toDate(formatInTimeZone(new Date(now.setHours(0, 0, 0, 0)), timeZone, 'yyyy-MM-dd\'T\'HH:mm:ss.SSSXXX'));
-  const endOfToday = toDate(formatInTimeZone(new Date(now.setHours(23, 59, 59, 999)), timeZone, 'yyyy-MM-dd\'T\'HH:mm:ss.SSSXXX'));
+  const startOfTodayZoned = toZonedTime(new Date(now.setHours(0, 0, 0, 0)), timeZone);
+  const endOfTodayZoned = toZonedTime(new Date(now.setHours(23, 59, 59, 999)), timeZone);
+
+  const startOfTodayUtc = fromZonedTime(startOfTodayZoned, timeZone);
+  const endOfTodayUtc = fromZonedTime(endOfTodayZoned, timeZone);
 
   const bookings = await db.booking.findMany({
     where: {
       date: {
-        gte: startOfToday,
-        lte: endOfToday,
+        gte: startOfTodayUtc,
+        lte: endOfTodayUtc,
       },
     },
     include: {
@@ -27,8 +30,56 @@ export const GetBookingOfDay = async () => {
   });
 
   console.log(`data atual: ${now}`);
-  console.log(`começo do dia (UTC): ${startOfToday}`);
-  console.log(`fim do dia (UTC): ${endOfToday}`);
+  console.log(`começo do dia (UTC): ${startOfTodayUtc}`);
+  console.log(`fim do dia (UTC): ${endOfTodayUtc}`);
   console.log(`agendamentos retornados: ${bookings}`);
   return bookings;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// "use server";
+
+// import { db } from "@/lib/prisma";
+
+// export const GetBookingOfDay = async () => {
+
+//   const nowUtc = new Date();
+//   const startOfToday = new Date();
+//   startOfToday.setHours(0, 0, 0, 0);
+
+//   const endOfToday = new Date();
+//   endOfToday.setHours(23, 59, 59, 999);
+
+//    const bookings = await db.booking.findMany({
+//     where: {
+//       date: {
+//         gte: startOfToday,
+//         lte: endOfToday,
+//       },
+//     },
+//     include: {
+//       service: true,
+//       user: true,
+//     },
+//     orderBy: {
+//       date: "asc",
+//     },
+//   }); 
+
+//   console.log(`data atual: ${nowUtc}`);
+//   console.log(`começo do dia: ${startOfToday}`);
+//   console.log(`fim do dia: ${endOfToday}`);
+//   console.log(`agendamentos retornados: ${bookings}`);
+//   return bookings;  
+  
+// };
