a6b0420b6a5954a5ca185e1f2c46d858d07aa30b
luizribeiro <ludehenriq@gmail.com>
2024-12-27 12:43:19 -0300

    refactor: ajustar a lógica de obtenção de agendamentos do dia para utilizar UTC

--- actions/get/getBookingOfDay.ts
+++ actions/get/getBookingOfDay.ts
@@ -3,16 +3,15 @@
 import { db } from "@/lib/prisma";
 
 export const GetBookingOfDay = async () => {
+  const nowUtc = new Date();
 
-  const now = new Date();
+  // Definindo o início do dia em UTC
+  const startOfToday = new Date(Date.UTC(nowUtc.getUTCFullYear(), nowUtc.getUTCMonth(), nowUtc.getUTCDate(), 0, 0, 0, 0));
 
-  const startOfToday = new Date();
-  startOfToday.setHours(0, 0, 0, 0);
+  // Definindo o fim do dia em UTC
+  const endOfToday = new Date(Date.UTC(nowUtc.getUTCFullYear(), nowUtc.getUTCMonth(), nowUtc.getUTCDate(), 23, 59, 59, 999));
 
-  const endOfToday = new Date();
-  endOfToday.setHours(23, 59, 59, 999);
-
-   const bookings = await db.booking.findMany({
+  const bookings = await db.booking.findMany({
     where: {
       date: {
         gte: startOfToday,
@@ -26,14 +25,13 @@ export const GetBookingOfDay = async () => {
     orderBy: {
       date: "asc",
     },
-  }); 
-  
-  console.log(`data atual: ${now}`);
-  console.log(`começo do dia: ${startOfToday}`);
-  console.log(`fim do dia: ${endOfToday}`);
+  });
+
+  console.log(`data atual: ${nowUtc}`);
+  console.log(`começo do dia (UTC): ${startOfToday}`);
+  console.log(`fim do dia (UTC): ${endOfToday}`);
   console.log(`agendamentos retornados: ${bookings}`);
-  return bookings;  
-  
+  return bookings;
 };
 
 
@@ -42,3 +40,44 @@ export const GetBookingOfDay = async () => {
 
 
 
+
+
+
+
+
+// "use server";
+
+// import { db } from "@/lib/prisma";
+
+// export const GetBookingOfDay = async () => {
+
+//   const nowUtc = new Date();
+//   const startOfToday = new Date();
+//   startOfToday.setHours(0, 0, 0, 0);
+
+//   const endOfToday = new Date();
+//   endOfToday.setHours(23, 59, 59, 999);
+
+//    const bookings = await db.booking.findMany({
+//     where: {
+//       date: {
+//         gte: startOfToday,
+//         lte: endOfToday,
+//       },
+//     },
+//     include: {
+//       service: true,
+//       user: true,
+//     },
+//     orderBy: {
+//       date: "asc",
+//     },
+//   }); 
+
+//   console.log(`data atual: ${nowUtc}`);
+//   console.log(`começo do dia: ${startOfToday}`);
+//   console.log(`fim do dia: ${endOfToday}`);
+//   console.log(`agendamentos retornados: ${bookings}`);
+//   return bookings;  
+  
+// };
