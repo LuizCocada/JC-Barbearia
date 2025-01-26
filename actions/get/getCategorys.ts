01d412b4d98423a4252ecccf660d7ecdc654416c
luizribeiro <ludehenriq@gmail.com>
2025-01-26 18:42:25 -0300

    refactor: corrigir nomenclatura de variáveis e otimizar a manipulação de categorias

--- actions/get/getCategorys.ts
+++ actions/get/getCategorys.ts
@@ -3,15 +3,15 @@
 import { db } from "@/lib/prisma";
 
 export const getCategorys = async () => {
-  const category = await db.category.findMany({});
+  const categorys = await db.category.findMany({});
 
-  category.map((category) => {
+  categorys.forEach((category) => {
     category.imageUrl = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(
       category.imageUrl
     )}`;
   });
 
-  return category;
+  return categorys;
 };
 
 
